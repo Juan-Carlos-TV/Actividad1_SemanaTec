b4003c39db5df2eb9d990451c4382dbda4bba056
Juan-Carlos-TV <a00827629@itesm.mx>
2020-10-27 12:54:42 -0600

    Documentación final

--- Actividad1_Pintar.py
+++ Actividad1_Pintar.py
@@ -5,21 +5,21 @@ from freegames import vector
 
 def line(start, end):
     "Draw line from start to end."
-    up()
-    goto(start.x, start.y)
-    down()
-    goto(end.x, end.y)
+    up()                                   #Pen up (levanta el lapiz)
+    goto(start.x, start.y)                 #Ubica a Start como punto de partida
+    down()                                 #Pen down (baja el lapiz)
+    goto(end.x, end.y)                     #Traza hasta el punto end
 
 def square(start, end):
     "Draw square from start to end."
     up()
-    goto(start.x, start.y)
+    goto(start.x, start.y)                 #Ubica Start como punto de partida
     down()
-    begin_fill()
+    begin_fill()                           #Comienza el llenado
 
-    for count in range(4):
-        forward(end.x - start.x)
-        left(90)
+    for count in range(4):                 #Se ejecuta el ciclo 4 veces al ser una figura de 4 lados
+        forward(end.x - start.x)           #Avanza de end.x a start.x
+        left(90)                           #Gira 90° (360°/4) a la izquierda
 
     end_fill()
 
@@ -66,38 +66,40 @@ def triangle(start, end):
     for count in range(3):         #Ejecuta el ciclo 3 veces al ser una figura de 3 lados
         forward(end.x - start.x)   #Cada lado medirá la diferencia entre el X de end y de start
         left(120)                  #Gira 120° (360°/3 Lados) a la izquierda
-    end_fill                       #Termina de llenar
+    end_fill()                     #Termina de llenar
 
 def tap(x, y):
     "Store starting point or draw shape."
-    start = state['start']
+    start = state['start']         #Extaer el punto de partida de state
 
-    if start is None:
-        state['start'] = vector(x, y)
+    if start is None:                 
+        state['start'] = vector(x, y)  #Si start es nulo, se le asigna el valor del punto clickeado
     else:
-        shape = state['shape']
-        end = vector(x, y)
-        shape(start, end)
-        state['start'] = None
+        shape = state['shape']         #Si start posee un valor se asigna a shape la función de la figura
+        end = vector(x, y)             #Se lee el punto end
+        shape(start, end)              #Se ejecuta la función de shape
+        state['start'] = None          #Se asigna un valor nulo a start
 
 def store(key, value):
     "Store value in state at key."
-    state[key] = value
+    state[key] = value                 #Guarda un valor dentro de state en base a una key
 
-state = {'start': None, 'shape': line}
-setup(420, 420, 370, 0)
-onscreenclick(tap)
+state = {'start': None, 'shape': line} #Inicializa state
+setup(420, 420, 370, 0)                #Establece el tamaño de la ventana (420 x 420) y su posición
+onscreenclick(tap)                     #Para leer los clicks en la pantalla se usará tap
 listen()
-onkey(undo, 'u')
-onkey(lambda: color('black'), 'K')
-onkey(lambda: color('white'), 'W')
-onkey(lambda: color('green'), 'G')
-onkey(lambda: color('blue'), 'B')
-onkey(lambda: color('red'), 'R')
+#Onkey asigna un evento/función a un caracter específico
+#Se usa lambda debido a que onkey requiere una función que no tenga parametros
+onkey(undo, 'u')                                #Asigna el comando undo a u
+onkey(lambda: color('black'), 'K')              #Asigna el color negro a la "K"
+onkey(lambda: color('white'), 'W')              #Asigna el color blanco a "W"
+onkey(lambda: color('green'), 'G')              #Asigna el color verde a "G"
+onkey(lambda: color('blue'), 'B')               #Asigna el color azul a "B"
+onkey(lambda: color('red'), 'R')                #Asigna el color red a "R
 onkey(lambda: color('violet'), 'V')             #Asigna el color violeta a la "V"
-onkey(lambda: store('shape', line), 'l')
-onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
-onkey(lambda: store('shape', rectangle), 'r')
-onkey(lambda: store('shape', triangle), 't')
-done()
+onkey(lambda: store('shape', line), 'l')        #Asigna la figura linea a "l"
+onkey(lambda: store('shape', square), 's')      #Asigna la figura de cuadrado a "s"
+onkey(lambda: store('shape', circle), 'c')      #Asigna la figura de círculo a "c"
+onkey(lambda: store('shape', rectangle), 'r')   #Asigna la figura de rectangulo a "r"
+onkey(lambda: store('shape', triangle), 't')    #Asigna la figura triagulo a "t"
+done()                                          #Comienza el loop de eventos
